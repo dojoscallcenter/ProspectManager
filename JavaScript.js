cbd0e42a74866dc8261d28dd71d5b9a793f91ba9
dojoscallcenter <61515113+dojoscallcenter@users.noreply.github.com>
2021-10-01 13:54:12 -0500

    Update JavaScript.js

--- JavaScript.js
+++ JavaScript.js
@@ -800,8 +800,8 @@ function addNewRecord(formObject){
     fetch(appURL,{
         method: 'POST', // *GET, POST, PUT, DELETE, etc.
         cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-        //mode: 'cors', 
-        //credentials: 'include',
+        mode: 'no-cors', 
+        credentials: 'include',
         redirect: 'follow', // manual, *follow, error
         body: JSON.stringify(formInfo) // body data type must match "Content-Type" header
       })
