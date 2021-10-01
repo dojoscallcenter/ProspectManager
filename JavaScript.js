207cb0804209d52de9e76210eecd80b8b0fcc620
dojoscallcenter <61515113+dojoscallcenter@users.noreply.github.com>
2021-10-01 13:52:27 -0500

    Update JavaScript.js

--- JavaScript.js
+++ JavaScript.js
@@ -5,7 +5,8 @@ let GoogleAuth;
 const SCOPE = 'https://www.googleapis.com/auth/script.send_mail https://www.googleapis.com/auth/drive https://www.googleapis.com/auth/spreadsheets https://www.googleapis.com/auth/calendar https://www.googleapis.com/auth/script.projects https://www.googleapis.com/auth/script.scriptapp https://www.googleapis.com/auth/script.deployments https://www.googleapis.com/auth/script.external_request https://www.googleapis.com/auth/presentations';
 const spreadsheetId = "1HdgnSTtfFPBr5obA2hyuhLOqdaA2bmqIShW9IKVWNn0"; //prosepect manager
 //ORIGINAL 
-const appURL = "https://script.google.com/macros/s/AKfycbyKg5mLu1i9U16Cq_GLRLe-j4UEQgAdlVDzEG6S-BQ70HYc7T_t350oH5Z9_C5pDrLd/exec";
+const appURL = "https://script.google.com/macros/s/AKfycbyCDw5q27hAzTXTFcPkQYwRPFIRCU0D8fYftC-pnZsgd-6dD6kCgUecgrOYbU7Pks8/exec"
+//const appURL = "https://script.google.com/macros/s/AKfycbyKg5mLu1i9U16Cq_GLRLe-j4UEQgAdlVDzEG6S-BQ70HYc7T_t350oH5Z9_C5pDrLd/exec"; LAST WORKING
 //const appURL = "https://script.google.com/a/macros/mydojos.com/s/AKfycbzsxmJeXvPY4cNQXGs1nX_I-O5iKJ1_yCgMaW4V8Ti0MXjLrmb8VePJvKAGmnJvIdd4/exec";
 var childRows = $('#newLeads tr.shown');
 var userFilter;
@@ -446,7 +447,8 @@ function sendWelcomeForm(d){
     $('#welcomeForm').prop("disabled",true);
     $('#welcomeForm').html('<span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span><span>Sending...</span>');
     //USE DEPLOYMENT ID INSTEAD OF SCRIPT ID
-    var scriptId = "AKfycbzsxmJeXvPY4cNQXGs1nX_I-O5iKJ1_yCgMaW4V8Ti0MXjLrmb8VePJvKAGmnJvIdd4";
+    //var scriptId = "AKfycbzsxmJeXvPY4cNQXGs1nX_I-O5iKJ1_yCgMaW4V8Ti0MXjLrmb8VePJvKAGmnJvIdd4"; OLD
+    var scriptId = "AKfycbyCDw5q27hAzTXTFcPkQYwRPFIRCU0D8fYftC-pnZsgd-6dD6kCgUecgrOYbU7Pks8";
     var formInfo = {
         add_constantcontact: "",
         age_class: d[12],
