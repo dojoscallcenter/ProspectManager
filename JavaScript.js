794f0dc9314b6eca6bf185b50896fa1470337b95
dojoscallcenter <61515113+dojoscallcenter@users.noreply.github.com>
2021-10-05 15:11:22 -0500

    Update JavaScript.js

--- JavaScript.js
+++ JavaScript.js
@@ -1722,7 +1722,7 @@ function newLeadsInit(){
                             action: function(dt){
                             //table.searchPanes.container().toggle();
                                 //You need to detach the container and prepend it to something in the table, I bet.
-                            table.searchPanes.container().show();
+                            table.searchPanes.container().detach().appendTo("#searchPanesPlaceholder").show();
                             }
                         }
                     },
